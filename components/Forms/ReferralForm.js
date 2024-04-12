5bc59331026ff965a2671a0db84472b98671dc8e
kirksurber2 <kirksurber1@Surber-App>
2024-04-12 12:07:58 -0500

    added next_public for env files and fetch

--- components/Forms/ReferralForm.js
+++ components/Forms/ReferralForm.js
@@ -21,7 +21,7 @@ export default function ReferralForm() {
         e.preventDefault();
         
         try {
-            const res = await fetch(`${process.env.URL}/api/marketing/referral`, {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/marketing/referral`, {
                 method: "POST",
                 headers: { "Content-Type": 'application/json' },
                 body: JSON.stringify(referral)
