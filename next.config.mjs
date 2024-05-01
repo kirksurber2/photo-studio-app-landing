f519c037a404e1b4151923e5a63e0e7b4572b9f4
kirksurber2 <kirksurber1@Surber-App>
2024-05-01 15:22:33 -0500

    added header options to config file and deleted middleware

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return[{
+            source: '/api/:path*',
+            headers: [
+                {key: 'Access-Control-Allow-Credentials', value: 'true'},
+                {key: 'Access-Control-Allow-Origin', value: 'https://photostudioapp.com'},
+                {key: 'Access-Control-Allow-Methods', value: 'GET, DELETE, POST, PUT, PATCH'},
+                {key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'}
+            ]
+        }]
+    }
+};
 
 export default nextConfig;
