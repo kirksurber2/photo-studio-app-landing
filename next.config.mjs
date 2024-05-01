9af6a386d30009ea74fb66626b015ecf942dde7e
kirksurber2 <kirksurber1@Surber-App>
2024-05-01 15:30:59 -0500

    changed config back to middleware

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async headers() {
-        return[{
-            source: '/api/:path*',
-            headers: [
-                {key: 'Access-Control-Allow-Credentials', value: 'true'},
-                {key: 'Access-Control-Allow-Origin', value: 'https://photostudioapp.com'},
-                {key: 'Access-Control-Allow-Methods', value: 'GET, DELETE, POST, PUT, PATCH'},
-                {key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'}
-            ]
-        }]
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
