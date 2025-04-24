2bea3a7054143a6cbaaefb89d9e21a1c6fc70bdb
Kirk Surber <kirk.surber@surberphotography.com>
2025-04-24 14:28:47 -0500

    traingin video and docs in production

---
+++ app/training/components/TrainingSection.jsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+
+export default function TrainingSection () {
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
---
+++ app/training/page.jsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+
+export default function TrainingPage () {
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
