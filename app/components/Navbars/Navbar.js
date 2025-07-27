7170d54ffbd72f1af91f274c4de73693f10f6c31
unknown <kirk.surber@surberphotography.com>
2025-07-27 13:06:22 -0500

    updated navbar for links

--- app/components/Navbars/Navbar.js
+++ app/components/Navbars/Navbar.js
@@ -1,51 +1,48 @@
 'use client'
-import { useState, useEffect } from 'react'
-import  Link  from 'next/link'
-
+import { useState } from 'react'
+import Link from 'next/link'
 import Image from 'next/image'
-import logo from '../../../assets/images/images/logoBlack-200.png'
 import styles from '../../components/Navbars/Navbar.module.css'
-import { LuUserCircle2, LuMenu } from "react-icons/lu";
+import logo from '../../../assets/images/images/logoBlack-200.png'
+import { LuUserCircle2, LuMenu } from 'react-icons/lu'
 import { FaGoogle, FaRegBell } from 'react-icons/fa'
 
+export default function Navbar() {
+  const [mobileMenu, setMobileMenu] = useState(false)
+  const [showDropdown, setShowDropdown] = useState(false)
 
+  return (
+    <div className={!mobileMenu ? styles.navbar : styles.mobileNavbar}>
+      <Link href='/'>
+        <Image src={logo} width={200} alt='logo image' />
+      </Link>
 
- export default function Navbar () {
-     
+      <Link href='/about'>About</Link>
+      <Link href='/features'>Features</Link>
 
-    const [mobileMenu, setMobileMenu] = useState(false)
-   
+      {/* Dropdown wrapper */}
+      <div
+        className={styles.dropdown}
+        onMouseEnter={() => setShowDropdown(true)}
+        onMouseLeave={() => setShowDropdown(false)}
+      >
+        <span className={styles.dropdownTitle}>Training & Education</span>
+        {showDropdown && (
+          <div className={styles.dropdownMenu}>
+            <Link href='/photography-training-and-education'>Overview</Link>
+            <Link href='/learn-photography'>Learn: Photography</Link>
+            <Link href='/learn-photography-business'>Learn: Business</Link>
+            <Link href='/learn-facebook'>Learn: Facebook Ads</Link>
+            <Link href='/business-leadership'>Business Leadership</Link>
+          </div>
+        )}
+      </div>
 
- 
+      <Link href='/refer-a-friend'>Refer-a-Friend</Link>
 
-
-
-
-    return (
-        
-        <div className={!mobileMenu? styles.navbar : styles.mobileNavbar}>    
-            <Link href='/'>
-                <Image src={logo} width={200} alt='logo image'/>
-            </Link>
-            
-
-            <Link href='/about'>About</Link>
-            <Link href='/features'>Features</Link>
-            <Link href='/learn-photography'>Learn Photography</Link>
-            <Link href='/learn-photography-business'>Learn Business</Link>
-            <Link href='/learn-facebook'>Learn Facebook Ads</Link>
-            <Link href='/refer-a-friend'>Refer-a-Friend</Link>
-            
-            
-            
-           
-            <Link href={'https://app.photostudioapp.com/login'}>
-                <button>
-                    Login
-                </button>
-            </Link>
-        </div>
-        
-    )
+      <Link href='https://app.photostudioapp.com/login'>
+        <button>Login</button>
+      </Link>
+    </div>
+  )
 }
-
