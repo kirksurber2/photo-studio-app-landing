ebbd884d400e37326113c667571c1447ffcc9aed
unknown <kirk.surber@surberphotography.com>
2025-07-27 11:28:56 -0500

    props

--- app/components/BlogIntroSection/BlogIntroSection.jsx
+++ app/components/BlogIntroSection/BlogIntroSection.jsx
@@ -1,27 +1,14 @@
-
-export default function BlogIntroSection ({title, intro, point1, point2, point3, point4, point5}){
-
-    return(
-        <div className="intro">
-            <h3>{title}</h3>
-            <p>{intro}</p>
-            <br/>
-            <br/>
-            <p>{point1}</p>
-            <br/>
-            <br/>
-            <p>{point2}</p>
-            <br/>
-            <br/>
-            <p>{point3}</p>
-            <br/>
-            <br/>
-            <p>{point4}</p>
-            <br/>
-            <br/>
-            <p>{point5}</p>
-            <br/>
-            <br/>
-        </div>
-    )
-}
+export default function BlogIntroSection(props) {
+    return (
+      <div className="intro">
+        <h3>{props.title}</h3>
+        <p>{props.intro}</p>
+        <p>{props.point1}</p>
+        <p>{props.point2}</p>
+        <p>{props.point3}</p>
+        <p>{props.point4}</p>
+        <p>{props.point5}</p>
+      </div>
+    );
+  }
+  
